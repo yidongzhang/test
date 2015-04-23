06d487056ac849f439d324821a653432ff16a6e3
yidong.zhang <yidong.zhang@spreadtrum.com>
2015-04-23 17:14:11 +0800

    Add do_while test

---
+++ c/do_while/main.c
@@ -0,0 +1,10 @@
+int main()
+{
+	int i = 0;
+	do {
+		i++;
+		printf("i=%d", i);
+		continue;
+	} while(i<10);
+
+}
---
+++ c/do_while/main.s
@@ -0,0 +1,33 @@
+	.file	"main.c"
+	.section	.rodata
+.LC0:
+	.string	"i=%d"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	movq	%rsp, %rbp
+	.cfi_offset 6, -16
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+.L2:
+	addl	$1, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	cmpl	$9, -4(%rbp)
+	jle	.L2
+	leave
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.4.7-1ubuntu2) 4.4.7"
+	.section	.note.GNU-stack,"",@progbits
