b5b684aa4d8be3273db6b6655d888007c37e9f4b
yidong.zhang <yidong.zhang@spreadtrum.com>
2015-04-24 09:59:26 +0800

    Add String split test

---
+++ java/Split_1.java
@@ -0,0 +1,12 @@
+public class Split_1 {
+
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+		String a = "ab|cd|e";
+		String[] b = a.split("|"); 
+		int i = b.length;
+		System.out.println(i);
+	
+        }
+}
+
